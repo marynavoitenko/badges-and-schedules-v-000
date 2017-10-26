0eaeca156b89d61c982d16217432e550ef016cbf
Maryna Voitenko <maryna.voitenko@gmail.com>
2017-10-26 00:44:02 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,11 @@
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(names)
+  badgearray = []
+  names.each do |name|
+    badgearray << name
+  end
+  badgearray
+end
